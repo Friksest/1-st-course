18830c64684ccce070561d14efefde18ef059c2d
Mark <63554654+Friksest@users.noreply.github.com>
2023-02-24 14:57:59 +0200

    Add files via upload

--- my-sign-up.py
+++ my-sign-up.py
@@ -1,13 +1,14 @@
 from os import system
 
-
 system("clear")
 def signUp(username, email, password):
     def ifLatinAndDigits(username):
-        latin_alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)] + [chr(i) for i in range(ord('A'), ord('Z')+1)]
+        latin_lower_alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+        latin_upper_alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+        # latin_alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)] + [chr(i) for i in range(ord('A'), ord('Z')+1)]
         digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         for char in username:
-            if char in latin_alphabet or char in digits:
+            if char in latin_lower_alphabet or char in latin_upper_alphabet or char in digits:
                 return True
             else:
                 return False
@@ -30,6 +31,6 @@ username = input("Add the username: ")
 email = input("Add the email: ")
 password = input("Add the password: ")
 
-
+# signUp(username, email, password)
 
 print(signUp(username, email, password))
